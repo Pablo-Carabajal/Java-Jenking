fd1cf82dedc509085d9d34ed6cc35ba196000df0
Pablo Carabajal <carabajalpabloezequiel@gmail.com>
2024-08-31 23:18:03 -0300

    prueba numero2

--- src/holaMundo.java
+++ src/holaMundo.java
@@ -1,6 +1,6 @@
 public class holaMundo {
     public static void main(String[] args) {
 
-        System.out.println("Hola mundo,como estan!");
+        System.out.println("hola mundo,como estan!!");
     }
 }
